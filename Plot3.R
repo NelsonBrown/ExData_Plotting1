c1cda77c3c48c9d5f091762c7279f354edec5224
Nelson Brown <NBrown@Viejas.com>
2014-10-08 10:22:14 -0700

    This version adds the plot3 files

--- Plot3.R
+++ Plot3.R
@@ -10,7 +10,14 @@ ds$Date = dates
 
 ds$DateTime =  strptime(paste(ds$Date, ds$Time),format = '%Y-%m-%d %H:%M:%S')
 
-hist(ds$Global_active_power,main = "Global Active Power", 
-     col = "red", xlab = "Global Active Power (kilowatts)")
+plot(ds$DateTime,ds$Sub_metering_1, type = "n",main = "", 
+     ylab = "Energy sub metering",xlab = "")
+points(ds$DateTime, ds$Sub_metering_1,type = "l")
+points(ds$DateTime, ds$Sub_metering_2,type = "l", col="red")
+points(ds$DateTime, ds$Sub_metering_3,type = "l", col= "blue")
+legend("topright", text.col = c("black", "red", "blue"), 
+       legend = c("----- Sub_metering_1",
+                  "----- Sub_metering_2",
+                  "----- Sub_metering_3"))
 dev.copy(png, file = "plot3.png", width =480, height=480)
 dev.off()
