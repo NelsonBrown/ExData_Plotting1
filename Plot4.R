a7a28182016b293c9ead653e1074fe237bd62188
Nelson Brown <NBrown@Viejas.com>
2014-10-08 10:35:46 -0700

    This version adds the plot4 files

--- Plot4.R
+++ Plot4.R
@@ -10,7 +10,22 @@ ds$Date = dates
 
 ds$DateTime =  strptime(paste(ds$Date, ds$Time),format = '%Y-%m-%d %H:%M:%S')
 
-hist(ds$Global_active_power,main = "Global Active Power", 
-     col = "red", xlab = "Global Active Power (kilowatts)")
-dev.copy(png, file = "plot4.png", width =480, height=480)
+par(mfcol = c(2,2))
+plot(ds$DateTime,ds$Global_active_power,main = "", type = "l",
+     xlab="", ylab = "Global Active Power (kilowatts)")
+plot(ds$DateTime,ds$Sub_metering_1, type = "n",main = "", 
+     ylab = "Energy sub metering",xlab = "")
+points(ds$DateTime, ds$Sub_metering_1,type = "l")
+points(ds$DateTime, ds$Sub_metering_2,type = "l", col="red")
+points(ds$DateTime, ds$Sub_metering_3,type = "l", col= "blue")
+legend("topright", text.col = c("black", "red", "blue"), 
+       legend = c("----- Sub_metering_1",
+                  "----- Sub_metering_2",
+                  "----- Sub_metering_3"))
+plot(ds$DateTime,ds$Voltage,main = "", type = "l",
+     xlab="datetime", ylab = "Voltage")
+plot(ds$DateTime,ds$Global_reactive_power,main = "", type = "l",
+     xlab="datetime", ylab = "Global Reactive Power")
+dev.copy(png, file = "
+         plot4.png", width =480, height=480)
 dev.off()
